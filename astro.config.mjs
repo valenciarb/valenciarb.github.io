8385b39b060fdf8938afbbfb72387ec25db3fb0f
Enrique Comba Riepenhausen <enrique@ecomba.pro>
2024-10-10 23:03:38 +0200

    removes the code plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import expressiveCode from "astro-expressive-code";
-import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-
-  integrations: [
-    expressiveCode({
-      themes: ["one-light"],
-      frames: {
-        // Example: Hide the "Copy to clipboard" button
-        showCopyToClipboardButton: false,
-      },
-      plugins: [pluginLineNumbers()],
-    }),
-  ],
-
+  adapter: cloudflare(),
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
@@ -28,6 +14,4 @@ export default defineConfig({
       prefixDefaultLocale: false,
     },
   },
-
-  adapter: cloudflare(),
 });
