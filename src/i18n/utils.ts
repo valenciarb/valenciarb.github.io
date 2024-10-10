5d30b2da7715048ce07c4096116fe600b215afeb
Enrique Comba Riepenhausen <enrique@ecomba.pro>
2024-10-10 23:43:03 +0200

    Fixes ts errors

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,23 +1,29 @@
-import { ui, defaultLang, showDefaultLang, routes } from './ui';
+import { ui, defaultLang, showDefaultLang, routes } from "./ui";
 
 export function getLangFromUrl(url: URL) {
-    const [, lang] = url.pathname.split('/');
-    if (lang in ui) return lang as keyof typeof ui;
-    return defaultLang;
+  const [, lang] = url.pathname.split("/");
+  if (lang in ui) return lang as keyof typeof ui;
+  return defaultLang;
 }
 
 export function useTranslations(lang: keyof typeof ui) {
-    return function t(key: keyof typeof ui[typeof defaultLang]) {
-        return ui[lang][key] || ui[defaultLang][key];
-    }
+  return function t(key: keyof (typeof ui)[typeof defaultLang]) {
+    return ui[lang][key] || ui[defaultLang][key];
+  };
 }
 
 export function useTranslatedPath(lang: keyof typeof ui) {
-    return function translatePath(path: string, l: string = lang) {
-        const pathName = path.replaceAll('/', '')
-        const hasTranslation = defaultLang !== l && routes[l] !== undefined && routes[l][pathName] !== undefined
-        const translatedPath = hasTranslation ? '/' + routes[l][pathName] : path
+  return function translatePath(path: string, l: keyof typeof ui = lang) {
+    const pathName = path.replaceAll("/", "");
+    // @ts-ignore
+    // const hasTranslation =
+    // defaultLang !== l && l in routes && pathName in routes[l];
+    // @ts-ignore
+    // const translatedPath = hasTranslation ? "/" + routes[l][pathName] : path;
+    const translatedPath = path;
 
-        return !showDefaultLang && l === defaultLang ? translatedPath : `/${l}${translatedPath}`
-    }
+    return !showDefaultLang && l === defaultLang
+      ? translatedPath
+      : `/${l}${translatedPath}`;
+  };
 }
