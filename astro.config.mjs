949bb973d6c81f9e0df923e2b3a054e78c573545
Enrique Comba Riepenhausen <enrique@ecomba.pro>
2024-10-16 08:57:50 +0200

    Removes the cloudflare adapter (for now)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
+//import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  adapter: cloudflare(),
+  //adapter: cloudflare(),
   site: "https://valenciarb.org",
   i18n: {
     defaultLocale: "es",
