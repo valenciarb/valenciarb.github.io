9f2b8fa6440d14f7cbc6937a54ba5a84f4115f50
Enrique Comba Riepenhausen <enrique@ecomba.pro>
2024-10-16 08:55:48 +0200

    Sets the output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   adapter: cloudflare(),
   site: "https://valenciarb.org",
   i18n: {
